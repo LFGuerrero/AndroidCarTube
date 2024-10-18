290baef3f9cbda84e16ee799781311b7897d1398
Luiz Felipe Guerrero <felipe.russi.guerrero@gmail.com>
2024-10-18 17:03:48 -0300

    lib youtube

--- automotive/build.gradle.kts
+++ automotive/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidyoutubeplayer)
     implementation(project(":shared"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
---
+++ automotive/src/main/java/com/example/androidcartube/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.androidcartube
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+}
