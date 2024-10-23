436352f22be1c12923334bab83ebb6eb77a4e261
Luiz Felipe Guerrero <felipe.russi.guerrero@gmail.com>
2024-10-23 16:13:55 -0300

    telas criadas

--- mobile/src/main/java/com/example/androidcartube/MainActivity.kt
+++ mobile/src/main/java/com/example/androidcartube/MainActivity.kt
@@ -5,10 +5,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.androidcartube.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        initView()
+    }
+    //uHB_bgS208U
 
+    private fun initView() {
+        binding.btnWatch.setOnClickListener {
+            binding.etVideoId.text?.toString()?.let {
+                startActivity(YouTubeWatch.newInstance(this, "uHB_bgS208U"))
+            }
+        }
     }
 }
---
+++ mobile/src/main/java/com/example/androidcartube/YouTubeWatch.kt
@@ -0,0 +1,49 @@
+package com.example.androidcartube
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidcartube.databinding.ActivityWatchBinding
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+
+
+class YouTubeWatch : AppCompatActivity() {
+    private lateinit var binding: ActivityWatchBinding
+    private lateinit var youTubePlayerView: YouTubePlayerView
+
+    private val videoId by lazy { this.intent.getStringExtra("videoId") }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWatchBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        initView()
+    }
+
+    private fun initView() {
+        youTubePlayerView = binding.youtubePlayerView
+        lifecycle.addObserver(youTubePlayerView)
+
+        youTubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+            override fun onReady(youTubePlayer: YouTubePlayer) {
+                youTubePlayer.loadVideo(videoId!!, 0f)
+            }
+        })
+
+        binding.backbutton.setOnClickListener {
+            finish()
+        }
+    }
+
+    companion object {
+        fun newInstance(context: Context, videoId: String): Intent {
+            val intent = Intent(context, YouTubeWatch::class.java)
+            intent.putExtra("videoId", videoId)
+            return intent
+        }
+    }
+}
